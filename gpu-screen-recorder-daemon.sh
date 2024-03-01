b06dc5e850c2375d1a9c72b7ed73effe7325de6c
mxve <68632137+mxve@users.noreply.github.com>
2024-03-01 10:00:09 +0100

    set quality to ultra (~1.5gb/5min)

--- gpu-screen-recorder-daemon.sh
+++ gpu-screen-recorder-daemon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while true; do
     date=$(date '+%Y-%m-%d_%H-%M-%S')
-    flatpak run --command=gpu-screen-recorder com.dec05eba.gpu_screen_recorder -a "desktop/$(pactl get-default-sink).monitor" -a "mic/$(pactl get-default-source)" -q very_high -w screen -cr full -f 60 -r 300 -c mp4 -o /mnt/ssd1/clips &> ~/scripts/logs/gpu-screen-recorder_${date}.log
+    flatpak run --command=gpu-screen-recorder com.dec05eba.gpu_screen_recorder -a "desktop/$(pactl get-default-sink).monitor" -a "mic/$(pactl get-default-source)" -q ultra -w screen -cr full -f 60 -r 300 -c mp4 -o /mnt/ssd1/clips &> ~/scripts/logs/gpu-screen-recorder_${date}.log
 done
